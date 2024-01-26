04ef46e0989bdda8d7a4bcbe98be255c5253d737
arafatGithubCode <md489288@gmail.com>
2024-01-26 12:10:59 +0600

    add update user profile api route

--- api/app.js
+++ api/app.js
@@ -1,10 +1,12 @@
 import express from "express";
+import cookieParser from "cookie-parser";
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
 
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
