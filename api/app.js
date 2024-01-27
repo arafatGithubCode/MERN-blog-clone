3c9629d9a9fda49d38fa607781fa0f1648cb51c2
arafatGithubCode <md489288@gmail.com>
2024-01-27 19:34:47 +0600

    add create post api route

--- api/app.js
+++ api/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import cookieParser from "cookie-parser";
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
+import postRouter from "./routes/post.route.js";
 
 const app = express();
 
@@ -10,6 +11,7 @@ app.use(cookieParser());
 
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
+app.use("/api/post", postRouter);
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
