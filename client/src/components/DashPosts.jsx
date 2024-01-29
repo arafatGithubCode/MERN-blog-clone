f929b052db831d19ee29d7b5cf6c573830529aeb
arafatGithubCode <md489288@gmail.com>
2024-01-29 19:14:03 +0600

    complete show more posts functionality

--- client/src/components/DashPosts.jsx
+++ client/src/components/DashPosts.jsx
@@ -1,12 +1,14 @@
 import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
-import { Table } from "flowbite-react";
+import { Spinner, Table } from "flowbite-react";
+import { toast } from "react-toastify";
 
 const DashPosts = () => {
   const { currentUser } = useSelector((state) => state.user);
   const [userPosts, setUserPosts] = useState([]);
   const [loadingPosts, setLoadingPosts] = useState(false);
+  const [showMore, setShowMore] = useState(true);
 
   useEffect(() => {
     const fetchPosts = async () => {
@@ -17,6 +19,9 @@ const DashPosts = () => {
         setLoadingPosts(false);
         if (res.ok) {
           setUserPosts(data.posts);
+          if (data.posts.length < 9) {
+            setShowMore(false);
+          }
         }
       } catch (error) {
         console.log(error.message);
@@ -26,10 +31,36 @@ const DashPosts = () => {
       fetchPosts();
     }
   }, [currentUser._id]);
-  console.log(userPosts);
+
+  const handleShowMorePosts = async () => {
+    const startIndex = userPosts.length;
+    try {
+      const res = await fetch(
+        `/api/post/getPosts?userId=${currentUser._id}&startIndex=${startIndex}`
+      );
+      const data = await res.json();
+      if (res.ok) {
+        setUserPosts((prev) => [...prev, data.posts]);
+        if (data.posts.length < 9) {
+          setShowMore(false);
+        }
+      }
+    } catch (error) {
+      toast.error("something went wrong!");
+      console.log(error.message);
+    }
+  };
+
   return (
     <div className="table-auto overflow-x-scroll md:mx-auto p-3 scrollbar scrollbar-track-slate-100 scrollbar-thumb-slate-300 dark:scrollbar-track-slate-700 dark:scrollbar-thumb-slate-500">
-      {loadingPosts && <h3>Loading...</h3>}
+      {loadingPosts && (
+        <div className="my-9 flex items-center gap-2">
+          <Spinner />
+          <span className="text-2xl font-semibold text-gray-700 dark:text-gray-300">
+            Loading...
+          </span>
+        </div>
+      )}
       {currentUser.isAdmin && !loadingPosts && userPosts.length > 0 ? (
         <>
           <Table hoverable className="shadow-md">
@@ -83,6 +114,14 @@ const DashPosts = () => {
               </Table.Body>
             ))}
           </Table>
+          {showMore && (
+            <button
+              onClick={handleShowMorePosts}
+              className="w-full text-center py-7 text-teal-500 hover:font-semibold"
+            >
+              Show More
+            </button>
+          )}
         </>
       ) : (
         <p>You have no posts yet!</p>
