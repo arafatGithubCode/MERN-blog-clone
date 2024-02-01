604b19b1f4f91345349a7713089d7c0f1fa0c9bd
arafatGithubCode <md489288@gmail.com>
2024-02-01 14:43:09 +0600

    complete home page

--- client/src/components/CallToAction.jsx
+++ client/src/components/CallToAction.jsx
@@ -4,7 +4,10 @@ const CallToAction = () => {
   return (
     <div className="flex flex-col sm:flex-row p-3 border border-teal-500 justify-center items-center rounded-tl-3xl rounded-br-3xl text-center">
       <div className="flex-1 justify-center flex flex-col">
-        <h2 className="text-2xl">Want to learn more about JavaScript?</h2>
+        <h2 className="text-2xl">
+          Want to learn HTML, CSS, JavaScript and MERN by building fun and
+          engaging projects?
+        </h2>
         <p className="text-gray-500 my-2">
           Checkout these resources with from my github repository
         </p>
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,5 +1,65 @@
+import { Link } from "react-router-dom";
+import CallToAction from "../components/CallToAction";
+import PostCard from "../components/PostCard";
+import { useEffect, useState } from "react";
 const Home = () => {
-  return <h1>home</h1>;
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const res = await fetch("/api/post/getPosts");
+        const data = await res.json();
+        if (res.ok) {
+          setPosts(data.posts);
+        }
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+    fetchPosts();
+  }, []);
+  return (
+    <div>
+      <div className="flex flex-col gap-5 py-28 p-3 max-w-6xl mx-auto">
+        <h1 className="text-3xl text-slate-800 dark:text-slate-400 font-bold lg:text-6xl">
+          Welcome to my Blog
+        </h1>
+        <p className="text-xs sm:text-sm text-gray-400">
+          Here you&apos;ll find a variety of articles and tutorials on topics
+          such as web development, software engineering, and programming
+          languages.
+        </p>
+        <Link
+          to="/search"
+          className="text-teal-600 font-medium hover:underline"
+        >
+          View all posts
+        </Link>
+      </div>
+      <div className="bg-amber-100 dark:bg-slate-700 p-3 z-50">
+        <CallToAction />
+      </div>
+      {posts && posts.length > 0 && (
+        <div className="flex flex-col">
+          <h1 className="text-2xl text-slate-800 dark:text-slate-300 text-center font-semibold py-7">
+            Recent Posts
+          </h1>
+          <div className="p-3 flex flex-wrap gap-5 justify-center items-center">
+            {posts.map((post) => (
+              <PostCard key={post._id} post={post} />
+            ))}
+          </div>
+          <Link
+            to="/search"
+            className="text-xl text-teal-600 hover:underline font-medium text-center py-5"
+          >
+            View all posts
+          </Link>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Home;
