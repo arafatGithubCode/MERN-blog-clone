2782c817bc1ffde71e938eb31b72e6435e0783ef
arafatGithubCode <md489288@gmail.com>
2024-02-02 11:59:20 +0600

    complete about page

--- client/src/layouts/Header.jsx
+++ client/src/layouts/Header.jsx
@@ -64,19 +64,21 @@ const Header = () => {
         </span>
         Blog
       </Link>
-      <form onSubmit={handleSubmit}>
-        <TextInput
-          type="text"
-          placeholder="Search..."
-          rightIcon={AiOutlineSearch}
-          className="hidden lg:inline"
-          value={searchTerm}
-          onChange={(e) => setSearchTerm(e.target.value)}
-        />
-      </form>
-      <Button className="w-12 h-10 lg:hidden" color="gray" pill>
-        <AiOutlineSearch />
-      </Button>
+      <Link to="/search">
+        <form onSubmit={handleSubmit}>
+          <TextInput
+            type="text"
+            placeholder="Search..."
+            rightIcon={AiOutlineSearch}
+            className="hidden lg:inline"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
+        </form>
+        <Button className="w-12 h-10 lg:hidden" color="gray" pill>
+          <AiOutlineSearch />
+        </Button>
+      </Link>
       <div className="flex gap-2 md:order-2">
         <Button
           className="w-12 h-10 hidden sm:inline"
--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,5 +1,32 @@
 const About = () => {
-  return <h1>About</h1>;
+  return (
+    <div className="min-h-screen flex justify-center items-center flex-col p-7">
+      <h1 className="text-slate-800 text-3xl lg:text-5xl font-semibold dark:text-slate-300">
+        About Arafat&apos;s Blog
+      </h1>
+      ;
+      <main className="space-y-7 max-w-3xl mx-auto text-justify text-gray-500">
+        <p>
+          Welcome to Arafat&apos;s Blog! This blog was created by Arafat
+          Ghavidel as a personal project to share his thoughts and ideas with
+          the world. Arafat is a passionate developer who loves to write about
+          technology, coding, and everything in between.
+        </p>
+        <p>
+          On this blog, you&apos;ll find weekly articles and tutorials on topics
+          such as web development, software engineering, and programming
+          languages. Arafat is always learning and exploring new technologies,
+          so be sure to check back often for new content!
+        </p>
+        <p>
+          We encourage you to leave comments on our posts and engage with other
+          readers. You can like other people&apos;s comments and reply to them
+          as well. We believe that a community of learners can help each other
+          grow and improve.
+        </p>
+      </main>
+    </div>
+  );
 };
 
 export default About;
