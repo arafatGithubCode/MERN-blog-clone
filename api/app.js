1f427c9e8c470d21422536f78eb654a7ad3c2e31
arafatGithubCode <md489288@gmail.com>
2024-02-02 15:29:37 +0600

    add pre installation

--- api/app.js
+++ api/app.js
@@ -4,16 +4,25 @@ import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
 import postRouter from "./routes/post.route.js";
 import commentRouter from "./routes/comment.route.js";
+import path from "path";
 
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
 
+const __dirname = path.resolve();
+
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/post", postRouter);
 app.use("/api/comment", commentRouter);
+
+app.use(express.static(path.join(__dirname, "/client/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
